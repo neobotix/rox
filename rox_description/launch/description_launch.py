8e6c91a9795b0d708c689b77b8085a2a4213e243
PRP <padhupradheep@gmail.com>
2023-12-06 19:59:56 +0100

    updating the xacross

--- rox_description/launch/description_launch.py
+++ rox_description/launch/description_launch.py
@@ -1,27 +1,37 @@
 # Neobotix GmbH
+# Author: Pradheep Padmanabhan
 
 import launch
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess, OpaqueFunction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir, LaunchConfiguration, Command, PathJoinSubstitution, FindExecutable, PythonExpression
 from launch_ros.actions import Node
+from launch.launch_context import LaunchContext
+from launch.conditions import IfCondition
 import os
 from pathlib import Path
+import xacro
 
-def generate_launch_description():
+def execution_stage(context: LaunchContext, frame_type):
+    frame_typ = frame_type.perform(context)
 
-    urdf = os.path.join(get_package_share_directory('rox_description'), 'urdf', 'rox.urdf')
+    urdf = os.path.join(get_package_share_directory('rox_description'), 'urdf', 'rox.urdf.xacro')
 
-    with open(urdf, 'r') as infp:  
-        robot_desc = infp.read()
-
-    # ToDo: Namespacing
     start_robot_state_publisher_cmd = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
         output='screen',
-        parameters=[{'robot_description': robot_desc}],
-		arguments=[urdf])
+        parameters=[{'robot_description': Command([
+            "xacro", " ", urdf, " ", 'frame_type:=',
+            frame_typ])}],
+        arguments=[urdf])
 
-    return LaunchDescription([start_robot_state_publisher_cmd])
+    return [start_robot_state_publisher_cmd]
+
+def generate_launch_description():
+    opq_function = OpaqueFunction(function=execution_stage, args=[LaunchConfiguration('frame_type',  default="short")])
+
+    return LaunchDescription([opq_function])
