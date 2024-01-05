d683541e77f3fac653edfc330bf1bcb3978bbfc0
PRP <padhupradheep@gmail.com>
2024-01-05 07:23:46 +0100

    user can now select the rox_type

--- rox_description/launch/description_launch.py
+++ rox_description/launch/description_launch.py
@@ -14,10 +14,15 @@ import os
 from pathlib import Path
 import xacro
 
-def execution_stage(context: LaunchContext, frame_type, arm_type):
+def execution_stage(context: LaunchContext, frame_type, rox_type, arm_type):
     frame_typ = frame_type.perform(context)
     arm_typ = arm_type.perform(context)
+    rox_typ = rox_type.perform(context)
 
+    if (rox_typ == "meca"):
+        frame_typ = "long"
+        print("Meca only supports long frame")   
+    
     urdf = os.path.join(get_package_share_directory('rox_description'), 'urdf', 'rox.urdf.xacro')
 
     start_robot_state_publisher_cmd = Node(
@@ -29,7 +34,9 @@ def execution_stage(context: LaunchContext, frame_type, arm_type):
             "xacro", " ", urdf, " ", 'frame_type:=',
             frame_typ,
             " ", 'arm_type:=',
-            arm_typ])}],
+            arm_typ,
+            " ", 'rox_type:=',
+            rox_type])}],
         arguments=[urdf])
 
     return [start_robot_state_publisher_cmd]
@@ -37,6 +44,7 @@ def execution_stage(context: LaunchContext, frame_type, arm_type):
 def generate_launch_description():
     opq_function = OpaqueFunction(function=execution_stage,
                                   args=[LaunchConfiguration('frame_type',  default="short"),
+                                        LaunchConfiguration('rox_type',  default="argo"),
                                         LaunchConfiguration('arm_type',  default="")])
 
     return LaunchDescription([opq_function])
