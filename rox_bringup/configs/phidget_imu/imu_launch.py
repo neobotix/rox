6c39515890747a9a4d654e23a401f8a52e916ffd
Pradheep <padhupradheep@gmail.com>
2024-03-06 16:30:12 +0100

    fixing IMU launch

--- rox_bringup/configs/phidget_imu/imu_launch.py
+++ rox_bringup/configs/phidget_imu/imu_launch.py
@@ -12,7 +12,7 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     robot_namespace = launch.substitutions.LaunchConfiguration('namespace', default="")
 
-    config = os.path.join(get_package_share_directory('neo_mpo_700-2'),'configs/phidget_imu','imu.yaml') 
+    config = os.path.join(get_package_share_directory('rox_bringup'),'configs/phidget_imu','imu.yaml') 
 
     imu_node = ComposableNodeContainer(
             name='phidget_container',
