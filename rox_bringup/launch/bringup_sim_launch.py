bb9158fb08bc1db35250c35d23e087af8523195b
Adrian Ricardez Ortigosa <adrian.ricardezortigosa@dlr.de>
2025-08-05 14:41:08 +0200

    removed IfCondition library

--- rox_bringup/launch/bringup_sim_launch.py
+++ rox_bringup/launch/bringup_sim_launch.py
@@ -7,7 +7,6 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction, AppendEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.actions import Node
 from launch.launch_context import LaunchContext
