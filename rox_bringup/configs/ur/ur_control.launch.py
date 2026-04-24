af9590148d60bc6df7ae50646faf11bb308dbebe
Pradheep <padhupradheep@gmail.com>
2026-04-24 13:16:56 +0200

    maintenance

--- rox_bringup/configs/ur/ur_control.launch.py
+++ rox_bringup/configs/ur/ur_control.launch.py
@@ -34,6 +34,7 @@ from launch.actions import (
     DeclareLaunchArgument,
     IncludeLaunchDescription,
     OpaqueFunction,
+    ExecuteProcess,
 )
 from launch.conditions import IfCondition, UnlessCondition
 from launch.launch_description_sources import AnyLaunchDescriptionSource
@@ -45,7 +46,7 @@ from launch.substitutions import (
 )
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterFile
-from launch_ros.substitutions import FindPackageShare
+from launch_ros.substitutions import FindPackagePrefix, FindPackageShare
 
 
 def launch_setup(context):
@@ -79,31 +80,53 @@ def launch_setup(context):
         output="screen",
     )
 
-    dashboard_client_node = Node(
-        package="ur_robot_driver",
+    dashboard_client_node = IncludeLaunchDescription(
         condition=IfCondition(
             AndSubstitution(launch_dashboard_client, NotSubstitution(use_mock_hardware))
         ),
-        executable="dashboard_client",
-        name="dashboard_client",
-        output="screen",
-        emulate_tty=True,
-        parameters=[{"robot_ip": robot_ip}],
+        launch_description_source=AnyLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [FindPackageShare("ur_robot_driver"), "launch", "ur_dashboard_client.launch.py"]
+            )
+        ),
+        launch_arguments={
+            "robot_ip": robot_ip,
+        }.items(),
     )
 
-    tool_communication_node = Node(
+    robot_state_helper_node = Node(
         package="ur_robot_driver",
-        condition=IfCondition(use_tool_communication),
-        executable="tool_communication.py",
-        name="ur_tool_comm",
+        executable="robot_state_helper",
+        name="ur_robot_state_helper",
         output="screen",
+        condition=UnlessCondition(use_mock_hardware),
         parameters=[
-            {
-                "robot_ip": robot_ip,
-                "tcp_port": tool_tcp_port,
-                "device_name": tool_device_name,
-            }
+            {"headless_mode": headless_mode},
+            {"robot_ip": robot_ip},
         ],
+    )
+
+    tool_comm_path = PathJoinSubstitution(
+        [
+            FindPackagePrefix("ur_client_library"),
+            "lib",
+            "ur_client_library",
+            "tool_communication.py",
+        ]
+    )
+
+    tool_communication_script = ExecuteProcess(
+        name="ur_tool_comm",
+        condition=IfCondition(use_tool_communication),
+        cmd=[
+            tool_comm_path,
+            robot_ip,
+            "--tcp-port",
+            tool_tcp_port,
+            "--device-name",
+            tool_device_name,
+        ],
+        output="screen",
     )
 
     urscript_interface = Node(
@@ -130,8 +153,21 @@ def launch_setup(context):
                     "force_torque_sensor_broadcaster",
                     "joint_state_broadcaster",
                     "speed_scaling_state_broadcaster",
+                    "tcp_pose_broadcaster",
                     "ur_configuration_controller",
                 ]
+            },
+        ],
+    )
+
+    trajectory_until_node = Node(
+        package="ur_robot_driver",
+        executable="trajectory_until_node",
+        name="trajectory_until_node",
+        output="screen",
+        parameters=[
+            {
+                "motion_controller": initial_joint_controller,
             },
         ],
     )
@@ -157,17 +193,27 @@ def launch_setup(context):
         "io_and_status_controller",
         "speed_scaling_state_broadcaster",
         "force_torque_sensor_broadcaster",
+        "tcp_pose_broadcaster",
         "ur_configuration_controller",
+        "friction_model_controller",
     ]
     controllers_inactive = [
         "scaled_joint_trajectory_controller",
         "joint_trajectory_controller",
         "forward_velocity_controller",
         "forward_position_controller",
+        "forward_effort_controller",
+        "force_mode_controller",
+        "passthrough_trajectory_controller",
+        "freedrive_mode_controller",
+        "tool_contact_controller",
     ]
     if activate_joint_controller.perform(context) == "true":
         controllers_active.append(initial_joint_controller.perform(context))
         controllers_inactive.remove(initial_joint_controller.perform(context))
+
+    if use_mock_hardware.perform(context) == "true":
+        controllers_active.remove("tcp_pose_broadcaster")
 
     controller_spawners = [
         controller_spawner(controllers_active),
@@ -177,9 +223,11 @@ def launch_setup(context):
     nodes_to_start = [
         control_node,
         dashboard_client_node,
-        tool_communication_node,
+        robot_state_helper_node,
+        tool_communication_script,
         controller_stopper_node,
         urscript_interface,
+        trajectory_until_node,
     ] + controller_spawners
 
     return nodes_to_start
@@ -194,12 +242,17 @@ def generate_launch_description():
             description="Type/series of used UR robot.",
             choices=[
                 "ur3",
-                "ur3e",
                 "ur5",
-                "ur5e",
                 "ur10",
+                "ur3e",
+                "ur5e",
+                "ur7e",
                 "ur10e",
+                "ur12e",
                 "ur16e",
+                "ur8long",
+                "ur15",
+                "ur18",
                 "ur20",
                 "ur30",
             ],
@@ -286,7 +339,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "controller_spawner_timeout",
-            default_value="300",
+            default_value="10",
             description="Timeout used when spawning controllers.",
         )
     )
@@ -299,6 +352,8 @@ def generate_launch_description():
                 "joint_trajectory_controller",
                 "forward_velocity_controller",
                 "forward_position_controller",
+                "freedrive_mode_controller",
+                "passthrough_trajectory_controller",
             ],
             description="Initially loaded robot controller.",
         )
